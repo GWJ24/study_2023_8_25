29f27e9a0675fe7e5fc8e6b5604862a0c8f5a643
WenJie Gan <2412304549@qq.com>
2023-08-26 08:52:41 +0800

    第一次测试提交

---
+++ Demo/src/Demo1.java
@@ -0,0 +1,124 @@
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+//主界面测试------------------
+public class Demo1 extends JFrame{
+    Demo1() {
+        Font f = new Font("华文行楷", Font.BOLD, 36);
+        //身份选择按钮
+        JRadioButton btn3=new JRadioButton("学生端");
+        JRadioButton btn4=new JRadioButton("教师端");
+        JRadioButton btn5=new JRadioButton("管理端");
+        JPanel p=new JPanel();
+        ButtonGroup pbtns=new ButtonGroup();
+        pbtns.add(btn3);
+        pbtns.add(btn4);
+        pbtns.add(btn5);
+        btn3.setFont(f);
+        btn4.setFont(f);
+        btn5.setFont(f);
+        btn3.setBackground(Color.WHITE);
+        btn4.setBackground(Color.WHITE);
+        btn5.setBackground(Color.WHITE);
+
+        btn3.setBounds(602,181,150,60);
+        btn4.setBounds(780,181,150,60);
+        btn5.setBounds(950,181,150,60);
+        add(btn3);
+        add(btn4);
+        add(btn5);
+        btn3.addActionListener(event->
+        {
+            System.out.println("选择了学生端");
+        });
+        btn4.addActionListener(event->
+        {
+            System.out.println("选择了教师端");
+        });
+        btn5.addActionListener(event->
+        {
+            System.out.println("选择了管理端");
+        });
+
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null); // 使用绝对定位
+
+        // 创建带有背景图片的JLabel
+        JLabel backgroundImageLabel = new JLabel(new ImageIcon("Demo/Image1/1.jpg"));
+        //获取当前屏幕的尺寸（长、宽的值）
+        Toolkit k = Toolkit.getDefaultToolkit();
+        Dimension d = k.getScreenSize();
+        backgroundImageLabel.setBounds(0, 0, 1250, 705);
+        //将当前窗口设置到屏幕正中央进行显示
+        setBounds(d.width / 2 - 1250 / 2, d.height / 2 - 705 / 2, 1250, 705);
+        backgroundImageLabel.setOpaque(false); // 设置背景透明
+
+
+        setResizable(false); //阻止用户拖拽改变窗口的大小
+
+
+        // 创建并设置JTextField的位置
+        //用户名输入框配置---------
+        JTextField f3 = new JTextField();
+        f3.setFont(f);
+        f3.setOpaque(false);
+        f3.setBounds(791, 267, 1077 - 791 + 2, 317 - 267 + 5);
+        add(f3);
+
+
+
+        //----密码输入框配置-------
+        JTextField f1 = new JTextField();
+        f1.setFont(f);
+        f1.setOpaque(false);
+        f1.setForeground(Color.BLACK);
+        f1.setCaretColor(Color.BLACK);
+        f1.setBounds(792, 360, 1077 - 791 + 2, 317 - 267 + 5);
+        add(f1);
+
+
+        //验证码输入框配置------------
+        Font f4 = new Font("Cambria", Font.BOLD, 36);
+        JTextField f2 = new JTextField();
+        f2.setFont(f4);
+        f2.setBounds(792, 442, 1077 - 791 + 2, 317 - 267 + 5);
+        f2.setOpaque(false);
+        f2.setForeground(Color.BLACK);
+        f2.setCaretColor(Color.BLACK);  //设置光标颜色
+        add(f2);
+
+        add( backgroundImageLabel);
+
+
+        //登录按钮配置
+        JButton btn1=new JButton(new ImageIcon("Demo/Image1/2.png"));
+        btn1.setBounds(733,528,111,54);
+        btn1.addActionListener(event->
+        {
+            System.out.println("跳转登陆界面");
+            Demo2 dd=new Demo2(this);
+        });
+        add(btn1);
+        btn1.setOpaque(false);
+
+
+        JButton btn2=new JButton(new ImageIcon("Demo/Image1/3.png"));
+        btn2.setBounds(926,529,112,54);
+        btn2.addActionListener(event->
+        {
+            System.out.println("跳转注册界面");
+        });
+        btn2.setOpaque(false);
+        add(btn2);
+        setVisible(true);
+
+
+    }
+    public static void main(String[] args) {
+        Demo1 d=new Demo1();
+    }
+}
---
+++ Demo/src/Demo2.java
@@ -0,0 +1,100 @@
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.*;
+
+public class Demo2 extends JFrame
+{
+    Demo2(Demo1 demo1)
+    {
+        demo1.setVisible(false);
+        Font f = new Font("华文行楷", Font.BOLD, 36);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null); // 使用绝对定位
+        //1.绘制背景
+        // 创建带有背景图片的JLabel
+        JLabel backgroundImageLabel = new JLabel(new ImageIcon("Demo/Image2/new.jpg"));
+        //获取当前屏幕的尺寸（长、宽的值）
+        Toolkit k = Toolkit.getDefaultToolkit();
+        Dimension d = k.getScreenSize();
+        backgroundImageLabel.setBounds(0, 0, 1161, 650);
+        //将当前窗口设置到屏幕正中央进行显示
+        setBounds(d.width / 2 - 1161/ 2, d.height / 2 - 650 / 2, 1161, 650);
+        //backgroundImageLabel.setOpaque(false); // 设置背景透明
+        add(backgroundImageLabel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1161,680);
+        setResizable(false);
+
+        //2.绘制退出按钮
+        //得到鼠标的坐标（用于推算对话框应该摆放的坐标）
+    /* backgroundImageLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                System.out.println("鼠标点击位置：X=" + x + ", Y=" + y);
+            }
+        });*/
+
+       JButton exit=new JButton("按钮");
+       exit.setBounds(977,515,121,57);
+       exit.setOpaque(false);
+       exit.addActionListener(event->
+       {
+           setVisible(false);
+           demo1.setVisible(true);
+       });
+       add(exit);
+
+       //绘制五大功能区域
+        JButton btn1=new JButton("按阿牛");
+        btn1.setBounds(135,262,272,82);
+        btn1.setOpaque(false);
+        btn1.addActionListener(event->
+        {
+            System.out.println("跳转图书管界面");
+        });
+        add(btn1);
+
+        JButton btn2=new JButton("按钮");
+        btn2.setBounds(459,242,219,80);
+        btn2.setOpaque(false);
+        btn2.addActionListener(event->
+        {
+            System.out.println("跳转选课界面");
+        });
+        add(btn2);
+
+
+        JButton btn3=new JButton("按钮");
+        btn3.setBounds(75,396,302,85);
+        btn3.setOpaque(false);
+        btn3.addActionListener(event->
+        {
+            System.out.println("跳转学籍管理界面");
+        });
+        add(btn3);
+
+        JButton btn4=new JButton("阿牛");
+        btn4.setOpaque(false);
+        btn4.setBounds(519,389,206,73);
+        btn4.addActionListener(event->
+        {
+            System.out.println("跳转宿舍界面");
+        });
+        add(btn4);
+
+        JButton btn5=new JButton("阿牛");
+        btn5.setBounds(258,517,196,86);
+        btn5.setOpaque(false);
+        btn5.addActionListener(event->
+        {
+            System.out.println("跳转商店界面");
+        });
+        add(btn5);
+        setVisible(true);
+
+
+    }
+}
